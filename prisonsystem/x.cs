bb2b3e6f43d0fcf4b5561bcf65828b6865446609
aminakbari7 <aminakbari733@gmail.com>
2023-12-04 09:30:53 +0330

    Create x.cs

---
+++ prisonsystem/x.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prisonsystem
+{
+    public partial class x : Form
+    {
+        prisonsystemEntities db = new prisonsystemEntities();
+        public x()
+        {
+            InitializeComponent();
+        }
+
+        private void x_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var q = from a in db.emploees.ToList()
+                    where a.lname.Contains(textBox1.Text) || a.fname.Contains(textBox2.Text) || a.job.Contains(textBox3.Text)
+                    select new { a.eid, a.fname, a.lname, a.birthday, a.job, a.salary };
+            dataGridView1.DataSource = q.ToList();
+        }
+    }
+}
